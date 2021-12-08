e4c0891e119cf6efd45c6752a2072791c7a2508b
amatmuhammad <muhrahmatdhyanf@gmail.com>
2021-12-08 21:48:33 +0800

    Upload File Python

---
+++ bruteforce.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as grafik
+
+x1 = int(input('Masukan nilai x1  = '))
+y1 = int(input('Masukan nilai y1  = '))
+x2 = int(input('Masukan nilai x2  = '))
+y2 = int(input('Masukan nilai y2  = '))
+
+print('=====================================================')
+
