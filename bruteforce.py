ff8c105fb67798af0bb12ce59c520e563df34026
sarmanchisarajji <77709695+sarmanchisarajji@users.noreply.github.com>
2021-12-08 23:18:53 +0800

    Menambahkan kondisi jika x1==x2

--- bruteforce.py
+++ bruteforce.py
@@ -7,3 +7,15 @@ x2 = int(input('Masukan nilai x2  = '))
 y2 = int(input('Masukan nilai y2  = '))
 
 print('=====================================================')
+
+x = x1
+y = y1
+
+if x1 == x2:
+    titik_A = []
+    titik_B = []
+    for i in range(0, y2, 1):
+        print('Garis yang di lewati oleh titik A da titik B yaitu : ', x, ',', y)
+        titik_A.append(x)
+        titik_B.append(y)
+        y=y+1
